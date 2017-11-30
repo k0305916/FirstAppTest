e9ec9e11b1fb484526d52b9c6a7bc2e431ba8fd4
Mr.Li <k03059164@sina.com>
2017-11-30 16:16:33 +0800

    first commit

---
+++ Test.py
@@ -0,0 +1,20 @@
+'''
+Created on 2017-8-28
+
+@author: fansli
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
+    def MyPrint(self):
+        print("Hello world")
+        return "hello world"
---
+++ testapp.py
@@ -0,0 +1,7 @@
+from Test import MyClass
+import math
+
+#print(dir(MyClass))
+#print(math.acos(3))
+my = MyClass(5)
+print(my.MyPrint())
